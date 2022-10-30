4a5980903c2730e7ba78d0643fe43c0a2721a2b0
Manishkumar8965 <k.manish72707@gmail.com>
2022-10-30 14:27:15 +0530

    Fire base connected

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         Btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                registerNewUser();
+                registerNewUser() ;
             }
         });
     }
